a648cc5aff09dc876a5396ef071e7260bd9573a9
s.mozghovoy <you@example.com>
2017-06-12 13:16:56 +0300

    project core

---
+++ softermii-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  softermii-test
+//
+//  Created by Sergey Mozgovoy on 12/06/2017.
+//  Copyright © 2017 Sergey Mozgovoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
